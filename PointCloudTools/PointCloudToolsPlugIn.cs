29e3e30ed0401341c0d579e33c43c99e72d87d45
Lando <lando.schumpich@gmail.com>
2019-08-11 10:14:36 +0200

    Projektdateien hinzufügen.

---
+++ PointCloudTools/PointCloudToolsPlugIn.cs
@@ -0,0 +1,29 @@
+﻿namespace PointCloudTools
+{
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class PointCloudToolsPlugIn : Rhino.PlugIns.PlugIn
+
+    {
+        public PointCloudToolsPlugIn()
+        {
+            Instance = this;
+        }
+
+        ///<summary>Gets the only instance of the PointCloudToolsPlugIn plug-in.</summary>
+        public static PointCloudToolsPlugIn Instance
+        {
+            get; private set;
+        }
+
+        // You can override methods here to change the plug-in behavior on
+        // loading and shut down, add options pages to the Rhino _Option command
+        // and maintain plug-in wide options in a document.
+    }
+}
